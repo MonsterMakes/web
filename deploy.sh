a1e652d4c3c35105de7b13d1080f23f2bb0059c3
JACOB LOCKEN <jlocken@gluenetworks.com>
2017-07-26 21:31:59 -0700

    Version 0.0.4 deploy script now deploys docker image.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 echo "*** STARTING DEPLOYMENT ***";
-ssh -oStrictHostKeyChecking=no -i id_rsa monster@monstermakes.tech;
-ll;
-echo "*** DEPLOYMENT COMPLETE ***";
-exit;
+ssh -oStrictHostKeyChecking=no -i id_rsa monster@monstermakes.tech << EOF
+  sleep 1
+  echo "Pulling Docker Container..."
+  docker pull lockenj/web
+  echo "Deploying Docker Container..."
+  docker run --name web -dit -p 80:81 --restart unless-stopped lockenj/web
+  echo "*** DEPLOYMENT COMPLETE ***"
+  exit
+EOF
